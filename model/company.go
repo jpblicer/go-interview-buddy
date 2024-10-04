29784bb025b07502cd08063610cf4ffb4b58d036
James Young <jpblicer@gmail.com>
2024-10-04 21:08:57 +0900

    create company struct

--- model/company.go
+++ model/company.go
@@ -1,5 +1,20 @@
 package model
 
+type Company struct {
+	Name string
+	Website string
+	InterviewStep int
+}
+
+// Create
+
+// Read All
 func ListCompanies(companies string) string {
 	return "Testing K.K."
 }
+
+// Read One
+
+// Delete
+
+
