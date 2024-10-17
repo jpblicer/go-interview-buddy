27f260e1e69f4422394f71e2cd48d8b2d3bac5b3
James Young <jpblicer@gmail.com>
2024-10-17 19:21:22 +0900

    create function to get user input and display string response

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("Welcome to Go Interview Buddy!")
@@ -8,4 +12,25 @@ func main() {
 	fmt.Println("1) Add a Company")
 	fmt.Println("2) List all Companies")
 	fmt.Println("3) Exit")
+
+	getUserInput()
+}
+
+func getUserInput() {
+	reader := bufio.NewReader(os.Stdin)
+
+	userInput, _ := reader.ReadString('\n')
+
+	fmt.Print(userInput)
+
+	if userInput == "1\n" {
+		fmt.Println("Add a Company")
+	} else if userInput == "2\n" {
+		fmt.Println("List all Companies")
+	} else if userInput == "3\n" {
+		fmt.Println("Exit")
+	} else {
+		main()
+	}
+
 }
