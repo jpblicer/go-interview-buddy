308e9bde434c8d57c77645c0ece85770d99f73f7
James Young <jpblicer@gmail.com>
2024-10-17 19:02:21 +0900

    create cli menu options

--- main.go
+++ main.go
@@ -4,4 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("Welcome to Go Interview Buddy!")
+	fmt.Println("Please choose an option:")
+	fmt.Println("1) Add a Company")
+	fmt.Println("2) List all Companies")
+	fmt.Println("3) Exit")
 }
